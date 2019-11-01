f79adaf8464dfc458319287bd2c304516a352aea
Wellnitj <michael.wellnitz22@gmail.com>
2019-11-01 15:23:09 -0400

    Structure outline only for python side

---
+++ Skeleton/skeleton.py
@@ -0,0 +1,32 @@
+#Class: Character
+
+#Class: Event ?? could just be a string
+
+#Class: Schedule ?? need someway to represent the schedule, class may not be necessary
+
+#Method: Schedule all available events
+
+#Method: Wait
+
+#Method: Observe
+
+#Method: Execute Events
+
+#Method: Tell Hearsay
+
+#Method: Query
+
+#Method: Reset
+
+#Hash: player/hearsay -> belief/goal
+
+#Value: Time
+
+#Value: Loop
+
+#More general tasks
+#IO processing, terminal based, nothing fancy
+
+#Triggereing an action in Ostari - may not be possible during run time, might require a run at each step
+
+#Reading beliefs and actions from ostari, also possibly not doable in runtime
