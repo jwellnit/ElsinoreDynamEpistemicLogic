fcbd8f8c34455db4768c01df896e651f0794f296
Wellnitj <michael.wellnitz22@gmail.com>
2019-11-09 17:56:57 -0500

    Event Construction Completed

--- Skeleton/skeleton.py
+++ Skeleton/skeleton.py
@@ -1,16 +1,47 @@
-#String List: List of Character
+#String List: List of Characters
+characters = ["Ophelia", "p1", "p2", "p3", "p4"]
+charLoc = {"Ophelia":"l1", "p1":"l1", "p2":"l2", "p3":"l3", "p4":"l4"}
 
 #Class: Event
+class Event:
+    """Contains all information for event"""
 
-#Class: Schedule ?? need someway to represent the schedule, class may not be necessary
+    def __init__(self, name, startTime, endTime, chars, loc, template):
+        self.name = name
+        self.startTime = startTime
+        self.endTime = endTime
+        self.chars = chars
+        self.loc = loc
+        self.template = template
+
+executeTemplate = "execute$name$($name$, $chars$, $loc$, observer(unit))"
+goTemplate = "go($chars$, $loc$)"
+
+#Manual Definitions, events
+e1 = Event("e1", 540, 600, ["p1", "p2"], "l2", executeTemplate)
+e2 = Event("e2", 2160, 2220, ["p1", "p2", "p3", "p4"], "l3", executeTemplate)
+e3 = Event("e3", 2160, 2220, ["p1", "p2", "p3", "p4"], "l4", executeTemplate)
+m1 = Event("m1", 2880, 2880, ["Ophelia", "p3"], "null", executeTemplate)
+go1 = Event("go1", 720, 720, ["p3"], "l2", goTemplate)
+go2 = Event("go2", 840, 840, ["p4"], "l2", goTemplate)
+
+#Priority Queue: Schedule
+defaultSchedule = [e1, go1, go2, m1]
+currentSchedule = defaultSchedule
+
+def AddToSchedule(event):
+
 
 #Method: Schedule all available events
 
 #Method: Wait
+def Wait(endTime):
+
 
 #Method: Observe
 
 #Method: Execute Events
+
 
 #Method: Tell Hearsay
 
@@ -21,8 +52,13 @@
 #Hash: player/hearsay -> belief/goal
 
 #Value: Time
+currentTime = 480
+
+def TextTimeToInt(time):
+    time_list =
 
 #Value: Loop
+currentLoop = 1
 
 #More general tasks
 #IO processing, terminal based, nothing fancy
@@ -32,4 +68,12 @@
 #world state, and then add more actions to it and rerun as the player makes more decisions, simulating
 #runtime action taking. This template is restored to the default on a loop reset.
 
+#Method: AddAction
+def AddAction(action):
+
+
+#Method: RunGame
+
 #Reading beliefs and actions from ostari, also possibly not doable in runtime - simply output from run game
+
+#Method QueryOutput
